a9c780a307f3328c6d06b8ed860452c5dbf0dd9f
hreniks <sbigor09@gmail.com>
2020-10-16 01:36:40 +0300

    completed 6 hard homework

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ let isNumber = function (n) {
 };
 
 
-function bot(){
+function bot(a){
 
     function ask(){
         let answer = +prompt('Угадай число от 1 до 100');
@@ -20,22 +20,32 @@ function bot(){
 
 
     const PersonNumber = ask();
+    let attempts = a;
 
+    if (attempts !== 0){
     if (PersonNumber === 0 || PersonNumber === null) return alert('Игра окончена!');
     else if (PersonNumber > number) {
-        alert('Загаданное число больше');
-        return bot();
+        alert('Загаданное число больше, осталось попыток: ' + attempts);
+        return bot(attempts--);
        
     }
     else if (PersonNumber < number){
-        alert('Загаданное число меньше');
-        return bot();
+        alert('Загаданное число меньше, осталось попыток: ' + attempts);
+        attempts--;
+        return bot(attempts--);
        
     }
     else if (PersonNumber === number){
-        return alert('Поздравляю, вы угадали!');
+        let personConfirm = confirm('Поздравляю, вы угадали!!! Хотите сыграть еще?');
+        if (personConfirm === false) return alert('Игра окончена!');
+        return bot(10);
+        
     }
+} else { 
+    let end = confirm('Попытки закончились, хотите сыграть еще?');
+    if (!end) return alert('Игра окончена!');
+    else return bot();
 
 }
-
-bot();
+}
+bot(10);
